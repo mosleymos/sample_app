06a8fb3fab774aca1dcb9405cf6b1ab8fd0eb069
Mori <keitamori@gmail.com>
2016-04-01 19:25:24 +0200

    Basic test pour la suite

--- spec/features/statics_spec.rb
+++ spec/features/statics_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 describe "Statics", type: :feature do
   it 'should show us home page of site' do
     visit '/'
-    expect(page).to have_content('Hello World')
+    expect(page).to have_content('CodeYou')
+    expect(page).to have_content('Sign in')
   end
   it 'should show us home page of site' do
     visit '/about'
     expect(page).to have_content('About')
+    expect(page).to have_content('Mosleymos')
+    expect(page).to have_link('https://github.com/mosleymos/simple_app')
   end
 
 end
---
+++ spec/models/users_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe User do
+  it 'should have a first name' do
+  end
+  it 'should have a last name' do
+  end
+end
